46198940776b433cdd32be158d7bef1a8737f9df
Muhammad Ilham Rijal Thaariq <ilhamthaariq@gmail.com>
2026-03-31 16:21:22 +0700

    Create exercise_3.dart

---
+++ Week 2/exercise_3.dart
@@ -0,0 +1,8 @@
+void main() {
+  int multiplier = 5;
+  
+  print("Multiplication Table for $multiplier:");
+  for (int i = 1; i <= 10; i++) {
+    print("$multiplier x $i = ${multiplier * i}");
+  }
+}
