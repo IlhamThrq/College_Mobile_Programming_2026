ee5001eab1a52a6355af96db9f121e1e9da24f15
Muhammad Ilham Rijal Thaariq <ilhamthaariq@gmail.com>
2026-03-31 16:19:20 +0700

    Create guided_practice.dart

---
+++ Week 2/guided_practice.dart
@@ -0,0 +1,9 @@
+void main() {
+  String name = "Muhammad Ilham Rijal Thaariq";
+  String studentId = "4.33.24.1.16";
+  String major = "D4-Teknologi Rekayasa Komputer";
+
+  print("Name: $name");
+  print("Student ID: $studentId");
+  print("Major: $major");
+}
