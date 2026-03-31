6162f2735988103ef71883203be04b37c47e1115
Muhammad Ilham Rijal Thaariq <ilhamthaariq@gmail.com>
2026-03-31 16:24:31 +0700

    Create latihan_2_suhu.dart

---
+++ Week 2/latihan_2_suhu.dart
@@ -0,0 +1,11 @@
+void main() {
+  double celcius = 30.0; 
+  
+  double reamur = (4 / 5) * celcius;
+  double fahrenheit = (9 / 5 * celcius) + 32;
+
+  print("=== Konversi Suhu ===");
+  print("Suhu Celcius: $celcius °C");
+  print("Suhu Reamur: $reamur °R");
+  print("Suhu Fahrenheit: $fahrenheit °F");
+}
