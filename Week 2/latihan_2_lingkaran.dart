4ea30f4aeeedf0c75e277c78de5cf32bcbc9e8c9
Muhammad Ilham Rijal Thaariq <ilhamthaariq@gmail.com>
2026-03-31 16:23:31 +0700

    Create latihan_2_lingkaran.dart

---
+++ Week 2/latihan_2_lingkaran.dart
@@ -0,0 +1,12 @@
+void main() {
+  double r = 10.0; 
+  double pi = 3.14159;
+  
+  double luas = pi * r * r;
+  double keliling = 2 * pi * r;
+  
+  print("=== Perhitungan Lingkaran ===");
+  print("Jari-jari: $r");
+  print("Luas Lingkaran: $luas");
+  print("Keliling Lingkaran: $keliling");
+}
